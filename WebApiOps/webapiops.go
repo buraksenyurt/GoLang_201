0ae7ceed33831ab7e11bf4376d17c064e8fc1f95
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-03-27 12:28:27 +0000

    Applied a few handlers

--- WebApiOps/webapiops.go
+++ WebApiOps/webapiops.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -10,8 +12,83 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":9000", nil)
+
+	// İlk Örnek
+	//http.HandleFunc("/", handler)
+	//err := http.ListenAndServe(":9000", nil)
+
+	// İkinci örnek
+	// var iRobot Robot
+	// err := http.ListenAndServe(":9000", iRobot)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// Üçüncü örnek
+	http.HandleFunc(apiRoot, func(w http.ResponseWriter, r *http.Request) {
+		msg := RobotApi{"Robot Api Root"}
+		output, err := json.Marshal(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprint(w, string(output))
+	})
+
+	http.HandleFunc(apiRoot+"/robots", func(w http.ResponseWriter, r *http.Request) {
+		robots := []Robot{
+			{Nickname: "Gemini", Region: "Earth Zone", Version: 1},
+			{Nickname: "WARP", Region: "Out of Space Zone", Version: 4},
+			{Nickname: "HAL - 2001", Region: "Black Hole Zone", Version: 99},
+		}
+		output, err := json.Marshal(robots)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprint(w, string(output))
+	})
+
+	http.HandleFunc(apiRoot+"/robots/me", func(w http.ResponseWriter, r *http.Request) {
+		robot := Robot{"T-1000", "Unknown Zone", 13}
+		output, err := json.Marshal(robot)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprint(w, string(output))
+	})
+
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println("Web Server is listening...")
 }
+
+type Robot struct {
+	Nickname string
+	Region   string
+	Version  int
+}
+
+func (r Robot) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.Nickname = "Robo IO 1001"
+	r.Region = "Red Zone"
+	r.Version = 1
+
+	req.ParseForm()
+	fmt.Println(req.Form)
+	fmt.Println("Path : ", req.URL.Path)
+	fmt.Fprintf(w, "<table style='border:1'><tr><td><b>Nickname</b></td><td><b>Region</b></td><td><b>Version</b></td></tr><tr><td>%s</td><td>%s</td><td>%d</td></tr></table>", r.Nickname, r.Region, r.Version)
+}
+
+type RobotApi struct {
+	Message string `json:"message"`
+}
+
+type Book struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Point int    `json:"int"`
+}
+
+const apiRoot string = "/api"
