16e07193da2199f4f7a79cf8750ff3b8ff12ebab
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-03-27 13:00:51 +0000

    Refactored test usages.

--- UnitTestOps/unittestops_test.go
+++ UnitTestOps/unittestops_test.go
@@ -14,11 +14,23 @@ import "testing"
 	FAIL
 	FAIL    example/unittestops     0.002s
 	FAIL
+
+	Tüm testleri görmek için  (başarılı olanlar da dahil) aşağıdaki gibi çalıştırabiliriz.
+	v = verbose anlamındadır
+	go test -v
 */
 
 func TestAdd(t *testing.T) {
 	got := Add(13, 12)
 	want := 10
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestAdd2(t *testing.T) {
+	got := Add(13, 12)
+	want := 25
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
 	}
